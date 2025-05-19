45afe09d3a969a81c3b24cca42ae5942ab917a48
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-05-19 07:54:41 +0100

    Task 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+number1 = 10
+number2 = 5
+
+sum = number1 + number2
+diff = number1 - number2
+multi = number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {diff}")
+print(f"Multiplication of {number1} and {number2} is {multi}")
+
