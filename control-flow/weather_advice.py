5c535e26e1897d4bae1ba61ec79cbaab688626ce
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-05-26 23:59:11 +0100

    Task 0

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-usr_input = str(input("What's the weather like today? (sunny/rainy/cold): "))
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-if (usr_input == "sunny"):
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif (usr_input == "rainy"):
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif (usr_input == "cold"):
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
-    print("I don't have recommendations for this weather.")
+    print("Sorry, I don't have recommendations for this weather.")
