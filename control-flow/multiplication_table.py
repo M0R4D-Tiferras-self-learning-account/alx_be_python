d4daf9fe2e32b6a1de8212cbaecb959b9a9ee02e
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-05-27 11:50:05 +0100

    Task 2

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,5 @@
 
 num = int(input("Enter a number to see its multiplication table: "))
 
-for it in range(11):
-    if (it > 0):
-        print(f"{num} * {it} = {num * it}")
+for it in range(1, 11):
+    print(f"{num} * {it} = {num * it}")
