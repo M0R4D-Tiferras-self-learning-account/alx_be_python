7f1c5ce483668d893bf995e203e51a2680225d92
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-06-08 14:46:35 +0100

    will be continued

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+def perform_operation(num1, num2, operation):
+    if (operation == "add"):
+        return (float(num1 + num2))
+    elif (operation == "subtract"):
+        return (float(num1 - num2))
+    elif (operation == "multiply"):
+        return (float(num1 * num2))
+    elif (operation == "divide"):
+        return (float(num1 / num2))
+
