e40cad98276fbfda3991320795dde2ccb53035b9
M0R4D-Tiferras-self-learning-account <m.tiferras@gmail.com>
2025-06-08 14:50:38 +0100

    will be continued

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,6 +7,9 @@ def perform_operation(num1, num2, operation):
         return (float(num1 - num2))
     elif (operation == "multiply"):
         return (float(num1 * num2))
-    elif (operation == "divide" and num2 != 0):
-        return (float(num1 / num2))
+    elif (operation == "divide"):
+        if num2 == 0:
+            return "Cannot divide by zero."
+        else:
+            return (float(num1 / num2))
 
